03cf620d7df45cb65a539e0d8680fea8f8db993c
chrise08 <chris.eckert08@gmail.com>
2020-09-21 10:10:21 -0500

    Day 2: Operators

--- src/Operators.java
+++ src/Operators.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Operators {
     // Complete the solve function below.
-    static int solve(double meal_cost, int tip_percent, int tax_percent) {
-        int tip = tip_percent / 100;
-        int tax = tax_percent / 100;
-        long totalCost = Math.round(meal_cost + tip + tax);
-        return (int) totalCost;
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        double tip = meal_cost * tip_percent / 100;
+        double tax = meal_cost * tax_percent / 100;
+        int totalCost = (int) Math.round(meal_cost + tip + tax);
+        System.out.println(totalCost);
     }
 
     private static final Scanner scanner = new Scanner(System.in);
