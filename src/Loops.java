891928712fc66b699151db7123df445aff2e5d49
chrise08 <chris.eckert08@gmail.com>
2020-09-22 17:52:02 -0500

    Day 5: Loops

---
+++ src/Loops.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Loops {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for (int i = 1; i <= 10; i++) {
+            int multiple = n * i;
+            System.out.printf("%d x %d = %d%n", n, i, multiple);
+        }
+        scanner.close();
+    }
+}
