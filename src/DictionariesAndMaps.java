c35727dd3bd6bb31a0ce9895bca709be0e6c6d6b
chrise08 <chris.eckert08@gmail.com>
2020-09-25 18:10:06 -0500

    Day 8: Dictionaries and Maps

---
+++ src/DictionariesAndMaps.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DictionariesAndMaps {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String, Integer> phoneBook = new HashMap<String, Integer>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            in.nextLine();
+            phoneBook.put(name, phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            if(phoneBook.get(s) == null)
+                System.out.println("Not found");
+            else {
+                System.out.println(s + "=" + phoneBook.get(s));
+            }
+        }
+        in.close();
+    }
+}
